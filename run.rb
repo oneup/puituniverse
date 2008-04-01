93af1c48a7fa5519033ed7502b6e16a056788de9
Florian Hufsky <flo@oneup.local>
2008-04-01 05:19:29 +0200

    yay

---
+++ run.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+# RUBY PREREQUISITS
+require "rubygems"
+require 'gosu'
+require "yaml"
+
+# PIMP MY RUBY
+require "from_future_import.rb"
+
+# LOAD THE UNIVERSE
+".".each_dir do |bundle|
+  require_all "#{bundle}"
+end
+
+# CODE GAMES!
+$game_name = ARGV[0] || $config['default_game']
+
+# PLAY!
+$game_class = $game_name.instantiate rescue quit("Your game class needs to be called #{$game_name.classify}")
+  
+$game = $game_class.new
+$game.show
+
