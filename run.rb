9743e475aca75ffe7e5c5078cfdbdb370ee4d092
Florian Hufsky <flo@oneup.local>
2008-04-15 02:09:44 +0200

    HotRuby backend stub and more

--- run.rb
+++ run.rb
@@ -6,25 +6,28 @@ require "yaml"
 require "from_future_import.rb"  # gemify this
 require "activeresource.rb"      # gemify this  
 
-# PIMP MY RUBY
-#begin                            # spwan a lisp like repl shell after exceptions
+def require_package folder
+  init_file = "#{folder}/init.rb"
+  require init_file if init_file.is_file?
+  require_all folder
+end
+    
+require "backend/init"           # core setup routine is special, so the usual each_dir require_all loop doesn't work
 
-  require "backend/init"      # core setup routine is special, so the usual each_dir require_all loop doesn't work
+# LOAD THE UNIVERSE
+".".each_dir do |bundle|
+  require_package bundle
+end
 
-  # LOAD THE UNIVERSE
-  ".".each_dir do |bundle|
-    require_all "#{bundle}"
-  end
+# CODE GAMES!
+$game_name = ARGV[0] || $config['default_game']
 
-  # CODE GAMES!
-  $game_name = ARGV[0] || $config['default_game']
+# PLAY!
+class_name = "#{$game_name.capitalize}_game"
+$game_class = class_name.instantiate #rescue quit("Your game class needs to be called #{class_name}")
 
-  # PLAY!
-  class_name = "#{$game_name.capitalize}_game"
-  $game_class = class_name.instantiate #rescue quit("Your game class needs to be called #{class_name}")
-  
-  $game = $game_class.new
-  $game.show
+$game = $game_class.new
+$game.show
 
 #rescue Exception => e  
 #  println "GRR, EXCEPTION!"
