22dbd948502a807541cd857fe867c5abb7cf57e1
Florian Hufsky <flo@oneup.local>
2008-04-18 06:31:36 +0200

    space invaders can be shot now

--- spaceinvaders/spaceinvaders.rb
+++ spaceinvaders/spaceinvaders.rb
@@ -24,65 +24,72 @@ end
 # now: other ships :)
 
 class EnemyShip < Gameobject    
-  def img
-    @img ||= "spaceinvaders/Space Invader#{@row}".img
-  end
-  
-  def width
-    img.width
-  end
-  
-  def height
-    img.height
-  end
-
   def initialize x, y, row
     @row = row
     @x = x
     @y = y
+    set_sprite("spaceinvaders/Space Invader#{@row}")
   end
   
   def update
     # todo: jump every 10 ticks
   end
+end
+
+class Shot < Gameobject
+  def initialize x, y
+    @x = x
+    @y = y
+    set_sprite "spaceinvaders/Shot1"
+  end
   
-  def draw
-    img.draw(@x, @y, 0)
+  def update
+    @y -= 5
+    
+    $game.all(EnemyShip).each do |enemy|
+      if self.collides_with? enemy # todo: why doesn't coldec work?
+        enemy.die
+      end
+    end
   end
 end
 
 class PlayerShip < Gameobject
   def initialize
-    @img = "spaceinvaders/Shooter".img
+    set_sprite("spaceinvaders/Shooter")
     @x = 0
-    @y = $game.height - @img.height - 10 # 10 pixels free to bottom
+    @y = $game.height - sprite.height - 10 # 10 pixels free to bottom
     @vel_x = 0
     @vel_y = 0
     
     map_keys(Gosu::Button::KbLeft => :move_left,
-      Gosu::Button::KbRight => :move_right,
-      Gosu::Button::GpLeft => :move_left,
-      Gosu::Button::GpRight => :move_right)
+             Gosu::Button::KbRight => :move_right,
+             Gosu::Button::GpLeft => :move_left,
+             Gosu::Button::GpRight => :move_right,
+             Gosu::Button::KbSpace => :shoot)
   end
   
-  def update    
+  def update
     @x += @vel_x
     @y += @vel_y
     @x = (0..$game.width).limit @x
     @y = (0..$game.height).limit @y
   end
   
+  def shoot pressed
+    if pressed
+      shot = Shot.new @x, @y - self.height
+      $game.objects << shot
+    end
+  end
+  
   def move_left pressed
-    @vel_x = -10 if pressed
+    @vel_x = -5 if pressed
     @vel_x = 0 if not pressed and not is_pressed? :move_right
   end
   
   def move_right pressed
-    @vel_x = 10 if pressed
+    @vel_x = 5 if pressed
     @vel_x = 0 if not pressed and not is_pressed? :move_left
-  end
-  
-  def draw
-    @img.draw(@x, @y, 0)
   end
 end
