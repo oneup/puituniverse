f5158aaba2e764a44f3e0fe4f4e3cf6c6320ae66
Florian Hufsky <florian.hufsky@gmail.com>
2008-11-19 12:26:01 +0100

    starting again

---
+++ _oncemorewithfeeling/init.rb
@@ -0,0 +1,20 @@
+# building the game thing from scratch, following http://code.google.com/p/gosu/wiki/RubyTutorial
+
+require "rubygems"
+require 'gosu'
+
+class GameWindow < Gosu::Window
+  def initialize
+    super(640, 480, false)
+    self.caption = "Gosu Tutorial Game"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
+
+window = GameWindow.new
+window.show
